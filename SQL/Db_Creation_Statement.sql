08eb670f58774524fda4a26aac06ef09f9cfc779
CarbonaraJack <linked.uno@gmail.com>
2017-11-23 15:26:09 +0100

    Fix lessicale allo statement del db, polish generale.

--- SQL/Db_Creation_Statement.sql
+++ SQL/Db_Creation_Statement.sql
@@ -1,52 +1,53 @@
-CREATE DATABASE Ritardi_Autobus;
-
 CREATE TABLE Utenti
-{
+(
 	UserID INTEGER PRIMARY KEY,
-	Username STRING(30),
-	Password STRING(30),
-	Email STRING(30),
-	Conto_Corrente STRING(30)
-};
+	User VARCHAR(50),
+	Pass VARCHAR(50),
+	Email VARCHAR(100)
+);
+
+CREATE TABLE Linea
+(
+	IdLinea INTEGER PRIMARY KEY,
+    NomeLinea VARCHAR(5)
+);
 
 CREATE TABLE Segnalazioni
-{
+(
 	IdSegnalazione INTEGER PRIMARY KEY,
 	IdSegnalatore INTEGER,
 	DataOra DATETIME,
 	Posizione POINT,
-	Linea STRING(5),
-	FOREIGN KEY (IdSegnalatore) REFERENCES Utenti(UserID)
-};
-
-CREATE TABLE Linea
-{
-	IdLinea INTEGER PRIMARY KEY
-};
+	Linea INTEGER,
+	FOREIGN KEY (IdSegnalatore) REFERENCES Utenti(UserID),
+    FOREIGN KEY (Linea) REFERENCES Linea(IdLinea)
+);
 
 CREATE TABLE Fermata
-{
+(
 	IdFermata INTEGER PRIMARY KEY,
+    NomeFermata VARCHAR(50),
 	Posizione POINT
-};
+);
 
 CREATE TABLE Linea_Fermata
-{
+(
 	IdLinea INTEGER,
 	IdFermata INTEGER,
 	FOREIGN KEY (IdLinea) REFERENCES Linea(IdLinea),
-	FOREIGN KEY (IdFermata) REFERENCES Fermata(IdFermata)
-};
+	FOREIGN KEY (IdFermata) REFERENCES Fermata(IdFermata),
+    primary key (IdLinea, IdFermata)
+);
 
 CREATE TABLE Autobus
-{
+(
 	IdAutobus INTEGER PRIMARY KEY,
 	IdLinea INTEGER,
 	FOREIGN KEY (IdLinea) REFERENCES Linea(IdLinea)
-};
+);
 
 CREATE TABLE Autobus_Fermata_Orario
-{
+(
 	IdAutobusFermataOrario INTEGER PRIMARY KEY,
 	IdAutobus INTEGER,
 	IdLinea INTEGER,
@@ -55,12 +56,12 @@ CREATE TABLE Autobus_Fermata_Orario
 	FOREIGN KEY (IdAutobus) REFERENCES Autobus(IdAutobus),
 	FOREIGN KEY (IdLinea) REFERENCES Autobus(IdLinea),
 	FOREIGN KEY (IdFermata) REFERENCES Fermata(IdFermata)
-};
+);
 
 CREATE TABLE Ritardi
-{
+(
 	IdRitardo INTEGER PRIMARY KEY,
 	IdAutobusFermataOrario INTEGER,
 	Ritardo TIME,
 	FOREIGN KEY (IdAutobusFermataOrario) REFERENCES Autobus_Fermata_Orario(IdAutobusFermataOrario)
-};
+);
