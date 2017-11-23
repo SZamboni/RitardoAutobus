b5e2707889bf4e92745406cfe45339006dff488b
CarbonaraJack <linked.uno@gmail.com>
2017-11-23 11:20:38 +0100

    Aggiunta dell'app definitiva che verrà runnata da Heroku. Primo abbozzo di funzione per segnalare i ritardi. Da aggiornare nel momento in cui abbiamo il Database

--- sqlEditor.node.js
+++ sqlEditor.node.js
@@ -1,4 +1,4 @@
-//semplice editor che permette di runnare query mysql
+//Connessione SQL
 var mysql      = require('mysql');
 var connection = mysql.createConnection({
   host     : 'ritardoautobus.c147tajn45vc.us-east-2.rds.amazonaws.com',
@@ -6,7 +6,12 @@ var connection = mysql.createConnection({
   password : 'trentino',
   database : 'ritardoautobus'
 });
+//Instanza Express
+var express = require('express');
+var app=express();
 
+/*
+//Query
 connection.connect();
 
 connection.query('SELECT * from test', function(err, rows, fields) {
@@ -17,3 +22,14 @@ connection.query('SELECT * from test', function(err, rows, fields) {
 });
 
 connection.end();
+*/
+var segnaRitardo = function(params){
+  var query = ('INSERT into qualcosa');
+  connection.connect();
+  connection.Query(query,function (errore){
+    if(!errore)
+      console.log('Aggiunta riga ai ritardi');
+    else
+      console.log('Errore nell\'aggiunta dei ritardi');
+  })
+}
