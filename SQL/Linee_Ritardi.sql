9e99fd927aa003d3442436304cd89d5af5a14df4
Chack96 <francesco.loda@studenti.unitn.it>
2017-11-27 15:58:53 +0100

    Creata la procedure Linee_Ritardi

---
+++ SQL/Linee_Ritardi.sql
@@ -0,0 +1,16 @@
+Create Procedure Linee_Ritardi(IN IdFerm INT,IN Scarto TIME)
+
+Select Linea.IdLinea,NomeLinea,Orario,Ritardo
+From
+(Select IdLinea,Orario,IdCorsa
+From Corsa_Fermata_Orario
+Where (IdLinea IN (SELECT IdLinea FROM Linea_Fermata WHERE IdFermata=IdFerm)) 
+And 
+(Orario between Time((Time(addtime(now(),'01:00:00')))) and (Time(addtime((Time(addtime(now(),'01:00:00'))),Scarto))))) As T1, Linea, Ritardo
+Where Linea.IdLinea IN (SELECT IdLinea FROM Linea_Fermata WHERE IdFermata=IdFerm)
+And 
+T1.IdCorsa=Ritardo.IdCorsa
+And
+Ritardo.DataRitardo = Curdate()
+And
+T1.IdLinea=Linea.IdLinea;
