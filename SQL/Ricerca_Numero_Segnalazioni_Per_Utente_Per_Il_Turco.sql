4882da432fed78493e7c5a82f4cacb590da60eac
Chack96 <francesco.loda@studenti.unitn.it>
2017-12-04 17:13:17 +0100

    Aggiunta query per calcolare i reward per il turco

---
+++ SQL/Ricerca_Numero_Segnalazioni_Per_Utente_Per_Il_Turco.sql
@@ -0,0 +1,4 @@
+Select Count(*) As NumeroSegnalazioni, IdSegnalatore, QualificationTypeId, Nome, Cognome
+From Segnalazione,Utente
+Where IdSegnalatore=UserID and Month(Date(DataOra))=Month(subdate(current_date, 1)) and Year(Date(DataOra))=Year(subdate(current_date, 1))
+Group by IdSegnalatore;
