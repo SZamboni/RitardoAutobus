6c0b6613fb74989f5471442fbadab43794ca4ce4
CarbonaraJack <linked.uno@gmail.com>
2017-12-05 14:52:40 +0100

    Aggiunta dei metodi (options in particolare) ai permessi CORS

--- app.node.js
+++ app.node.js
@@ -39,6 +39,7 @@ var pMedia=0.2;
 /** Abilito CORS su tutto il server **/
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods","POST, GET, PUT, DELETE, OPTIONS");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
