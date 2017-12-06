46e9ce5759ec1c52af00cbde92397f9983a226be
Chack96 <francesco.loda@studenti.unitn.it>
2017-12-06 19:16:09 +0100

    Creation Statement aggiornato

--- SQL/Select_Generale.sql
+++ SQL/Select_Generale.sql
@@ -4,6 +4,6 @@ SELECT * FROM Fermata;
 SELECT * FROM Corsa;
 SELECT * FROM Corsa_Fermata_Orario;
 SELECT * FROM Linea_Fermata;
-SELECT * FROM Segnalazione where SegnalazioneValida=1;
+SELECT * FROM Segnalazione;
 SELECT * FROM Ritardo;
 SELECT * FROM Utenti_Hit_Id;
