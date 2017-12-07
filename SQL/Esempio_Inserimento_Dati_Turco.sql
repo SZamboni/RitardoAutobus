f520e298aad93e27eae59e993a60a41b83583ec1
Chack96 <francesco.loda@studenti.unitn.it>
2017-12-07 18:28:35 +0100

    Modifiche varie ed aggiunta di alcune query per il Turco

--- SQL/Esempio_Inserimento_Dati_Turco.sql
+++ SQL/Esempio_Inserimento_Dati_Turco.sql
@@ -2,5 +2,30 @@ update Utente
 set WorkerId='xxxx'
 where UserID=111;
 
-insert into Utenti_Hit_Id(UserID,UtentiHitId,DataHit)
-values (111,'adadada',curdate());
+insert into Utenti_Hit(UserID,HitId,HitTypeId,DataHit)
+values (11,'ggggdddds','hhhsfdsssf','2017-05-07');
+
+update Utenti_Hit
+set UserID=6
+where UserID=11;
+
+-- Numero di Hit non visualizzati in base all'UserId
+-- Dato l'Id : Tutto
+-- Update che mette visualizzato a 1 dato l'Id della tabella
+-- Update che setta elaborato a 1 da l'Id
+
+Select count(*)
+From Utenti_Hit
+Where UserId=1;
+
+Select *
+From Utenti_Hit
+Where UserId=1;
+
+Update Utenti_Hit
+Set Visualizzato=1
+Where UtentiHitId=1;
+
+Update Utenti_Hit
+Set Elaborato=1
+Where Utenti_Hit=1;
--- SQL/Linee_Ritardi.sql
+++ SQL/Linee_Ritardi.sql
@@ -1,6 +1,6 @@
 Create Procedure Linee_Ritardi(IN IdFerm INT,IN Scarto TIME)
 
-Select Linea.IdLinea,NomeLinea,Orario,Ritardo,T1.IdCorsa
+Select distinct Linea.IdLinea,NomeLinea,Orario,Ritardo,T1.IdCorsa
 From
 (Select IdLinea,Orario,IdCorsa
 From Corsa_Fermata_Orario
@@ -13,4 +13,25 @@ T1.IdCorsa=Ritardo.IdCorsa
 And
 Ritardo.DataRitardo = Curdate()
 And
-T1.IdLinea=Linea.IdLinea;
+T1.IdLinea=Linea.IdLinea
+Order By Orario,IdCorsa,IdLinea;
+
+Select * From Fermata Where NomeFermata like '%manci%';
+
+Drop Procedure Linee_Ritardi;
+
+Call Linee_Ritardi(10405,'00:10:00');
+
+Select distinct IdLinea,Orario,IdCorsa
+From Corsa_Fermata_Orario
+Where (IdLinea IN (SELECT IdLinea FROM Linea_Fermata WHERE IdFermata=11))
+And 
+(Orario between Time((Time(addtime(now(),'01:00:00')))) and (Time(addtime((Time(addtime(now(),'01:00:00'))),'00:04:00'))))
+Order by Orario;
+
+Select * From Corsa_Fermata_Orario Where IdCorsa=26805 order by Orario;
+
+Alter Table Utenti_Hit_Id
+Add column HitTipeId VARCHAR(100);
+
+Describe Utenti_Hit_Id;
