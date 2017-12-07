8ee98f4940e5786f5cf3a97097fe0a233ec490c5
CarbonaraJack <linked.uno@gmail.com>
2017-12-07 11:34:46 +0100

    Set vars per heroku

--- app.node.js
+++ app.node.js
@@ -670,8 +670,8 @@ app.use(function (request, response) {
 });
 //apro server su porta 8080
 //heroku vuole ascoltare sulla sua porta
-//var porta = process.env.PORT || 3000;
-var porta = 8080;
+var porta = process.env.PORT || 3000;
+//var porta = 8080;
 app.listen(porta, function () {
     console.log('Server aperto');
 });
