45d0175eddd57ef0a89a838ca823a1cc5f538b95
Anna Martignano <martignano.anna@gmail.com>
2017-12-08 11:10:39 +0100

    fixed insertion of special characters

--- insert_data.sql
+++ insert_data.sql
@@ -1,35 +1,41 @@
 -- 1 -- routes
 LOAD DATA LOCAL INFILE 'C:/ProgramData/MySQL/MySQL Server 5.7/Uploads/routes.txt' INTO TABLE routes
+  CHARACTER SET UTF8
   FIELDS TERMINATED BY ',' ENCLOSED BY '"'
   LINES TERMINATED BY '\r\n'
   IGNORE 1 LINES;
 
 -- 2 -- calendar
-LOAD DATA LOCAL INFILE 'C:/ProgramData/MySQL/MySQL Server 5.7/Uploads/calendar.txt' INTO TABLE calendar 
+LOAD DATA LOCAL INFILE 'C:/ProgramData/MySQL/MySQL Server 5.7/Uploads/calendar.txt' INTO TABLE calendar
+  CHARACTER SET UTF8
   FIELDS TERMINATED BY ',' ENCLOSED BY '"'
   LINES TERMINATED BY '\r\n'
   IGNORE 1 LINES;
   
 -- 3 -- calendar_dates
-LOAD DATA LOCAL INFILE 'C:/ProgramData/MySQL/MySQL Server 5.7/Uploads/calendar_dates.txt' INTO TABLE calendar_dates 
+LOAD DATA LOCAL INFILE 'C:/ProgramData/MySQL/MySQL Server 5.7/Uploads/calendar_dates.txt' INTO TABLE calendar_dates
+  CHARACTER SET UTF8
   FIELDS TERMINATED BY ',' ENCLOSED BY '"'
   LINES TERMINATED BY '\r\n'
   IGNORE 1 LINES;
 
 -- 4 -- stops
 LOAD DATA LOCAL INFILE 'C:/ProgramData/MySQL/MySQL Server 5.7/Uploads/stops.txt' INTO TABLE stops
+  CHARACTER SET UTF8
   FIELDS TERMINATED BY ',' ENCLOSED BY '"'
   LINES TERMINATED BY '\r\n'
   IGNORE 1 LINES;
 
 -- 5 -- trips
-LOAD DATA LOCAL INFILE 'C:/ProgramData/MySQL/MySQL Server 5.7/Uploads/trips.txt' INTO TABLE trips 
+LOAD DATA LOCAL INFILE 'C:/ProgramData/MySQL/MySQL Server 5.7/Uploads/trips.txt' INTO TABLE trips
+  CHARACTER SET UTF8
   FIELDS TERMINATED BY ',' ENCLOSED BY '"'
   LINES TERMINATED BY '\r\n'
   IGNORE 1 LINES;
 
 -- 6 -- stop_times
 LOAD DATA LOCAL INFILE 'C:/ProgramData/MySQL/MySQL Server 5.7/Uploads/stop_times.txt' INTO TABLE stop_times
+  CHARACTER SET UTF8
   FIELDS TERMINATED BY ',' ENCLOSED BY '"'
   LINES TERMINATED BY '\r\n'
   IGNORE 1 LINES;
