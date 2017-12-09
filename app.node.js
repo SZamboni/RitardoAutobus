23def099912e5bc06cbb8d555dfac928bc804deb
CarbonaraJack <linked.uno@gmail.com>
2017-12-09 18:53:37 +0100

    Fatti 3 test

--- app.node.js
+++ app.node.js
@@ -617,7 +617,7 @@ var resetRitardi = function(){
     }
   })
 };
-var scheduleRitardi = schedule.scheduleJob({hour: 00, minute: 00},resetRitardi);
+var scheduleRitardi = schedule.scheduleJob('1 0 */1 * *',resetRitardi);
 /**
 Funzione che viene chiamata ogni intervalloRitardi millisecondi per aggiornare
 la tabella dei ritardi a partire dalla tabella delle segnalazioni.
---
+++ app.node.test.js
@@ -0,0 +1,78 @@
+const request = require('supertest');
+const app = require('./app.node.js');
+
+describe('Testo il root', () => {
+    test('Dovrebbe darmi 200', (done) => {
+        request(app).get('/').then((response) => {
+            expect(response.statusCode).toBe(200);
+            done();
+        });
+    });
+});
+
+describe('Testo una pagina inesistente', () => {
+    test('Dovrebbe darmi 404', (done) => {
+        request(app).get('/sijdhfsjdhg').then((response) => {
+            expect(response.statusCode).toBe(404);
+            done();
+        });
+    });
+});
+
+describe('Testo il get delle fermate', () => {
+    test("Dovrebbe darmi le fermate attorno a me", (done) => {
+        request(app)
+        .get("/fermate/?latitude=46.0659276&longitude=11.1548419&scanRange=\'02:00:00\'")
+        .then((response) => {
+          expect(response.statusCode).toBe(200);
+          expect(response.body.length).not.toBe(0);
+          done();
+        });
+    });
+});
+/*
+describe('Testo il get delle fermate con parametri sbagliati', () => {
+    test("Dovrebbe darmi 500 internal server error", (done) => {
+        request(app)
+        .get("/fermate/?latitude=46.0659276")
+        .then((response) => {
+            expect(response.statusCode).toBe(500);
+          done();
+        });
+    });
+});
+*/
+describe('Testo il get dei ritardi della fermata di povo piazza manci', () => {
+    test("Dovrebbe darmi i ritardi della fermata", (done) => {
+        request(app)
+        .get("/ritardi/?idFermata=170&rangeTempo=\'02:00:00\'")
+        .then((response) => {
+          expect(response.statusCode).toBe(200);
+          expect(response.body.length).not.toBe(0);
+          done();
+        });
+    });
+});
+/*
+describe('Testo il get dei ritardi della fermata con parametri sbagliati', () => {
+    test("Dovrebbe darmi internal server error 500", (done) => {
+        request(app)
+        .get("/ritardi/?idFermata'")
+        .then((response) => {
+          expect(response.statusCode).toBe(500);
+          done();
+        });
+    });
+});
+describe('--------', () => {
+    test("-------------", (done) => {
+        request(app)
+        .get("----------------")
+        .then((response) => {
+          expect(response.statusCode).toBe(200);
+          expect(response.body.length).not.toBe(0);
+          done();
+        });
+    });
+});
+*/
