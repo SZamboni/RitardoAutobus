69fe143907865233bbb4f61c0455f3744389c188
Chack96 <francesco.loda@studenti.unitn.it>
2017-12-09 19:43:24 +0100

    Aggiunti 5 casi di test per getFermate e getRitardi

--- app.node.test.js
+++ app.node.test.js
@@ -1,6 +1,8 @@
 const request = require('supertest');
 const app = require('./app.node.js');
 
+// Testing di Marcello
+
 describe('Testo il root', () => {
     test('Dovrebbe darmi 200', (done) => {
         request(app).get('/').then((response) => {
@@ -71,6 +73,65 @@ describe('Testo il get dei ritardi della fermata senza range temporale', () => {
         .get("/ritardi/?idFermata=170'")
         .then((response) => {
           expect(response.statusCode).toBe(500);
+          done();
+        });
+    });
+});
+
+// Testing di Francesco
+
+describe('Testo il get dei ritardi della fermata con il tempo che non è del tipo giusto', () => {
+    test("Dovrebbe darmi internal server error 500", (done) => {
+        request(app)
+        .get("/ritardi/?idFermata=170&rangeTempo=abc")
+        .then((response) => {
+          expect(response.statusCode).toBe(500);
+          done();
+        });
+    });
+});
+
+describe('Testo il get dei ritardi della fermata senza parametri', () => {
+    test("Dovrebbe darmi errore", (done) => {
+        request(app)
+        .get("/ritardi/")
+        .then((response) => {
+          expect(response.statusCode).toBe(500);
+          done();
+        });
+    });
+});
+
+describe('Testo il get delle fermate senza parametri', () => {
+    test("Dovrebbe darmi errore", (done) => {
+        request(app)
+        .get("/fermate/")
+        .then((response) => {
+          expect(response.statusCode).toBe(500);
+          done();
+        });
+    });
+});
+
+describe('Testo il get dei ritardi della fermata con un parmetro in più', () => {
+    test("Dovrebbe darmi 200 perchè scarta il parametro aggiuntivo", (done) => {
+        request(app)
+        .get("/ritardi/?idFermata=170&rangeTempo=\'02:00:00\'&prova=abc")
+        .then((response) => {
+          expect(response.statusCode).toBe(200);
+          expect(response.body.length).not.toBe(0);
+          done();
+        });
+    });
+});
+
+describe('Testo il get delle fermate con un parametro in più', () => {
+    test("Dovrebbe darmi le fermate attorno a me perchè ignora il parametro aggiuntivo", (done) => {
+        request(app)
+        .get("/fermate/?latitude=46.0659276&longitude=11.1548419&scanRange=\'02:00:00\'&aggiuntivo=42")
+        .then((response) => {
+          expect(response.statusCode).toBe(200);
+          expect(response.body.length).not.toBe(0);
           done();
         });
     });
